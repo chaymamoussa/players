066c8d7a01584ef2a50250305f4a7e89069a74e7
Chayma Moussa <chayma.moussa@ensi-uma.tn>
2022-03-14 10:18:26 +0100

    add endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,34 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+import * as bodyParser from "body-parser";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+const main = express();
+
+main.use("players", app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({extended: false}));
+
+const db = admin.firestore();
+export const playersApi = functions.https.onRequest(main);
+
+interface Player
+{
+    id: string,
+    name: string,
+    number: string,
+    team: string
+}
+
+app.post('/players', async (req, res) => {
+  const player: Player = {
+    id: "1",
+    name: "Cristiano Ronaldo",
+    number: "7",
+    team: "Manchester United",
+  };
+  await db.collection("player").add(player);
+});
